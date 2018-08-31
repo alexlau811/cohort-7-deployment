40a779800d464de6a07b6ef0abe511552afef2e6
Alex Lau <alexlau811@gmail.com>
2018-08-31 15:43:49 +0800

    Add router

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,20 @@ import './App.css';
 
 import logo from './logo.svg';
 
+import { Route } from 'react-router';
+
+const Test1 = () => {
+  return (
+    <div>Test1</div>
+  )
+};
+
+const Test2 = () => {
+  return (
+    <div>Test2</div>
+  )
+};
+
 class App extends React.Component {
   public render() {
     return (
@@ -14,6 +28,8 @@ class App extends React.Component {
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <Route path="/test1" component={Test1} />
+        <Route path="/test2" component={Test2} />
       </div>
     );
   }
